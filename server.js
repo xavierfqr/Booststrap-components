6d5d7006c2c2dc194db2d3c89c85d66005c584eb
Xavier <facqueur.xavier@gmail.com>
2021-04-16 16:34:44 +0200

    server root

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const path = require('path')
 const app = express()
 
 app.use('/', serverStatic(path.join(__dirname, '/dist')))
+app.get("*", function(req, res) {
+    res.sendFile(__dirname + "/dist/index.html")
+})
 
 const port = process.env.PORT || 8080
 app.listen(port)
